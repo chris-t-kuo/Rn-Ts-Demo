1eb0734ef159d2a83f0deb190413a97c52a32ffe
ck77 <kuo.sunnyfruit@gmail.com>
2021-02-16 12:51:26 +0800

    123

--- src/component/SettingScreen.tsx
+++ src/component/SettingScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import Base from '../style/base';
+import { Navigation } from 'react-native-navigation';
 
 const SettingScreen = (props: any) => {
+
+    const handlePress = () => {
+        Navigation.pop(props.componentId);
+    }
+
     return (
         <View style={Base.root}>
             <Text>Settings Screen</Text>
+            <Button onPress={handlePress} title="Go Back" />
         </View>
     );
 };
